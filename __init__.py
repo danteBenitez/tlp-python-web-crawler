a7aa27573d5100c2636ff6ac39dceca65957b2bb
danteBenitez <benitezdante123@gmail.com>
2024-05-15 12:05:34 -0300

    fix(cli): Comportamiento al pasarle parámetros

--- __init__.py
+++ __init__.py
@@ -1,32 +1,43 @@
 from crawler import crawl
+from log import log_error
 import json
 import sys
 
 DEFAULT_URL = "https://docs.astro.build/es/getting-started"
 
+def print_help():
+    log_error("""
+       Uso: python -m crawler [options] [URL]
+       URL: La URL de la página web que deseas analizar
+            Si no se proporciona una URL, se utilizará la URL por defecto
+       Options:
+            --help Muestra este mensaje de ayuda
+            --verbose Muestra mensajes de depuración
+    """)
+
 def main():
     """
         Lógica general para acceder al Web Crawler mediante un CLI.
     """
+    verbose = False
     if "--verbose" in sys.argv:
         print("Iniciando el Web Crawler", file=sys.stderr)
+        verbose = True
+        sys.argv.remove("--verbose")
     elif "--help" in sys.argv:
-        print("Uso: python -m crawler [URL]", file=sys.stderr)
-        print("URL: La URL de la página web que deseas analizar", file=sys.stderr)
-        print("--help Muestra este mensaje de ayuda", file=sys.stderr)
-        print("--verbose Muestra mensajes de depuración", file=sys.stderr)
+        print_help()
+        sys.argv.remove("--help")
         return
 
     url = DEFAULT_URL
     if len(sys.argv) > 1:
         url = sys.argv[1]
 
-    results = crawl(url)
+    results = crawl(url, verbose=verbose)
     with open('results.json', 'w') as file:
         json.dump(results, fp=file)
 
     print("Resultados guardados en results.json")
 
 if __name__ == "__main__":
-
     main()
